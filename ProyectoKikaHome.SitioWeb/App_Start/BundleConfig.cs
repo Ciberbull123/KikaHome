491792e632677c2173e68bf91d9a8fb88c95a3bd
Sergio Siácara <ing.sergio.siacara@outlook.com>
2024-11-06 20:11:36 -0400

    Actualización de proyecto Web - ASP.NET
    Actualización del proyecto

---
+++ ProyectoKikaHome.SitioWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ProyectoKikaHome.SitioWeb
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+
+            bundles.Add(new StyleBundle("~/Content/kika").Include(
+          "~/Content/kika.css"));
+
+            bundles.Add(new Bundle("~/bundles/kika").Include(
+                      "~/Scripts/gsap.min.js",
+                      "~/Scripts/ScrollTrigger.min.js",
+                      "~/Scripts/kika.js"));
+
+        }
+    }
+}
